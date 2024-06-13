9761c5788c401f81262d2fe779cd51222723cc7d
cmdenis <christianmdenis@gmail.com>
2024-06-13 15:47:38 -0400

    added text, schematic picture and more cursors

--- sliders_update.js
+++ sliders_update.js
@@ -35,3 +35,42 @@ pC2Slider.oninput = function() {
     plotSim(c1, c2)
 }
 
+// Slider for simulation parameter p1
+let p1Slider = document.getElementById("p1Slider");
+document.getElementById("p1").innerHTML = p1Slider.value;
+p1 = p1Slider.value
+
+
+p1Slider.oninput = function() {
+    document.getElementById("p1").innerHTML = this.value;
+    p1 = this.value
+    plotSim(c1, c2)
+}
+
+
+// Slider for simulation parameter p2
+let p2Slider = document.getElementById("p2Slider");
+document.getElementById("p2").innerHTML = p2Slider.value;
+p2 = p2Slider.value
+
+
+p2Slider.oninput = function() {
+    document.getElementById("p2").innerHTML = this.value;
+    p2 = this.value
+    plotSim(c1, c2)
+}
+
+// Slider for simulation parameter p3
+let p3Slider = document.getElementById("p3Slider");
+document.getElementById("p3").innerHTML = p3Slider.value;
+p3 = p3Slider.value
+
+
+p3Slider.oninput = function() {
+    document.getElementById("p3").innerHTML = this.value;
+    p3 = this.value
+    plotSim(c1, c2)
+}
+
+
+
